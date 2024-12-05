1d69088a710907557bcb7fd0302e91d6005e1a01
unknown <ustolaunknown@gmail.com>
2024-12-06 04:05:01 +0530

    all done2

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -76,7 +76,7 @@ const Features = () => {
         </div>
         <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh] ">
           <BentoCard
-            src="/public/videos/feature-1.mp4"
+            src="/videos/feature-1.mp4"
             title={
               <>
                 radi<b>n</b>t
@@ -88,21 +88,21 @@ const Features = () => {
         <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
             <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
                 <BentoCard 
-                    src='/public/videos/feature-2.mp4'
+                    src='/videos/feature-2.mp4'
                     title={<>Zig<b>m</b>a</>}
                     description={'An anime and gaming-inspired NFT collection - the IP primed for expension'}
                 />
             </BentoTilt>
             <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0 ">
                 <BentoCard 
-                    src={'/public/videos/feature-3.mp4'}
+                    src={'/videos/feature-3.mp4'}
                     title={<>N<b>E</b>XUS</>}
                     description={'A gaminfied social hub, adding a new dimension of play to social intrection for Web3 communication.'}
                 />
             </BentoTilt>
             <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
                 <BentoCard 
-                    src={'/public/videos/feature-4.mp4'}
+                    src={'/videos/feature-4.mp4'}
                     title={<>A<b>Z</b>UL</>}
                     description={'A cross-world AI Agent - elevating your gameplay to be more fun and productive.'}
                 />
@@ -115,7 +115,7 @@ const Features = () => {
             </div>
             <div className="bento-tilt_2">
                 <video 
-                    src="/public/videos/feature-5.mp4"
+                    src="/videos/feature-5.mp4"
                     
                     loop
                     muted
